ff3a32a7f7aac13888706b6b0513ddf9ec2869f4
Normz <johnnormancapule@gmail.com>
2012-08-24 12:47:04 +0800

    WIP fixing css of registration pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require jquery-ui-1.8.23.custom.min.js
+//= require_directory .
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Bhs::Application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, 
+               :controllers => {
+                 :registrations => "users/registrations",
+                 :sessions => "users/sessions"
+               }
   devise_scope :user do
-    get "/login" => "devise/sessions#new"
-    delete "/logout" => "devise/sessions#destroy"
+    match 'login' => 'users/sessions#new'
+    match 'logout' => 'users/sessions#destroy'
   end
   root :to => "pages#index"
 end
